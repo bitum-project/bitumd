e8dddcec299d35336c8405a037b368ade4ef1dc3
fonero-project <support@fonero.org>
2019-09-22 09:33:24 +0500

    update

--- blockchain/validate.go
+++ blockchain/validate.go
@@ -568,7 +568,8 @@ func checkBlockHeaderSanity(header *wire.BlockHeader, timeSource MedianTimeSourc
 	// A block must not contain fewer votes than the minimum required to
 	// reach majority once stake validation height has been reached.
 	if header.Height >= stakeValidationHeight {
-		majority := (chainParams.TicketsPerBlock / 2) + 1
+		majority := 1
+		//(chainParams.TicketsPerBlock / 2) + 1
 		if header.Voters < majority {
 			errStr := fmt.Sprintf("block does not commit to enough "+
 				"votes (min: %d, got %d)", majority,
