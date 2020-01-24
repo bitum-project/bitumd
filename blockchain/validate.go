8f89db47fd6c026266e949089494cb42690ad086
Bitum Project <50862214+bitum-project@users.noreply.github.com>
2020-01-24 13:01:12 +0500

    Update validate.go

--- blockchain/validate.go
+++ blockchain/validate.go
@@ -571,9 +571,10 @@ func checkBlockHeaderSanity(header *wire.BlockHeader, timeSource MedianTimeSourc
 	// A block must not contain fewer votes than the minimum required to
 	// reach majority once stake validation height has been reached.
 	if header.Height >= stakeValidationHeight && header.Height > 19385  {
-		majority := uint16(0)
-		if(header.Height > 29999) {
-			majority = (chainParams.TicketsPerBlock / 2) + 1
+		if(header.Height < 2999999) {
+			majority := uint16(0)
+		} else {
+			majority := (chainParams.TicketsPerBlock / 2) + 1
 		}
 		if header.Voters < majority {
 			errStr := fmt.Sprintf("block does not commit to enough "+
